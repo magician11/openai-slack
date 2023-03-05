0065b079d0abdc894d46a62215f0f821c7b4207b
magician11 <support@andrewgolightly.com>
2023-03-05 15:50:21 +0530

    giving in channel options

--- functions/index.js
+++ functions/index.js
@@ -5,5 +5,9 @@ export const slack = functions.https.onRequest(async (request, response) => {
   const prompt = request.body.text;
   functions.logger.log('prompt', prompt);
   const text = await createCompletion(prompt);
-  response.send(`*${prompt}*\n${text}`);
+
+  response.json({
+    // response_type: 'in_channel', // uncomment if you want in_channel responses (public) https://api.slack.com/interactivity/slash-commands#responding_immediate_response
+    text: `*${prompt}*\n${text}`
+  });
 });
