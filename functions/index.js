63d208c6493ec8611113c059a4241d38b9c2242e
A <support@andrewgolightly.com>
2023-06-07 06:42:11 -0400

    Updated index.js
    to match online tutorial

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,33 @@
-import functions from 'firebase-functions';
+import { onRequest } from 'firebase-functions/v2/https';
+import { info } from 'firebase-functions/logger';
 import got from 'got';
 import { createCompletion } from './modules/openai.js';
 
-export const slack = functions.https.onRequest(async (request, response) => {
-  functions.logger.log(request.body);
+export const helloWorld = onRequest(async (request, response) => {
+  info(`${request.body.command} ${request.body.text}`, request.body);
 
-  // just to check that calls to my Firebase Function are coming from my Slack account
-  // https://api.slack.com/authentication/verifying-requests-from-slack
-  if (request.body.token !== process.env.SLACK_TOKEN) {
-    response.status(401).send('Sorry, you do not have the required access.');
-  } else {
-    // send an immediate response to Slack within 3 seconds
-    // https://api.slack.com/interactivity/handling#acknowledgment_response
-    response.send('Let me think about this...');
+  // Confirming receipt
+  response.send('Let me think about this...');
 
-    const prompt = request.body.text;
-    functions.logger.log('prompt', prompt);
-    const text = await createCompletion(prompt);
+  try {
+    // get response from OpenAI
+    const openAiResponse = await createCompletion(request.body.text);
+    info(`prompt: ${request.body.text}`, {
+      prompt: request.body.text,
+      completion: openAiResponse
+    });
 
-    // send the completion from OpenAI when it's done
-    // https://api.slack.com/interactivity/handling#message_responses
-    const { data } = await got
-      .post(request.body.response_url, {
-        json: {
-          // response_type: 'in_channel', // uncomment if you want in_channel responses (public) https://api.slack.com/interactivity/slash-commands#responding_immediate_response
-          text: `*${prompt}*\n${text}`
-        }
-      })
-      .json();
-
-    functions.logger.log(data);
+    // Send a follow up message to Slack
+    await got.post(request.body.response_url, {
+      json: {
+        text: `*${request.body.text}*\n\n${openAiResponse}`
+      }
+    });
+  } catch (error) {
+    await got.post(request.body.response_url, {
+      json: {
+        text: 'Oops, there was an error with OpenAI.'
+      }
+    });
   }
 });
